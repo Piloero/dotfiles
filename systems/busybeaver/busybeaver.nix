886d71bb9cbe145fabe1fabaf1ffa3a6b56fea65
Piloero <piloero@proton.me>
2025-07-29 07:11:32 +0000

    feat: added x11 on busybeaver

--- systems/busybeaver/busybeaver.nix
+++ systems/busybeaver/busybeaver.nix
@@ -34,14 +34,27 @@
     (builtins.readFile /home/nixos/.secrets/RootCA.crt)
   ];
 
-
   nixpkgs.config.allowUnfree = true;
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
 
   environment.systemPackages = with pkgs; [
     wget
     vim
     obsidian
     git
+    jetbrains.pycharm-community
   ];
 
   programs.nix-ld = {
