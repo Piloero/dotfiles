69debdc29d10ef6528534046721155530c255069
Piloero <piloero@proton.me>
2025-08-30 16:23:08 +0200

    feat: setup flatpak and added bambulabstudio

---
+++ modules/desktop/apps/bambulab.nix
@@ -0,0 +1,21 @@
+
+{
+  config,
+  pkgs,
+  pkgs-unstable,
+  ...
+}:
+
+{
+  environment.systemPackages = with pkgs; [
+    (bambu-studio.overrideAttrs (previousAttrs: {
+      version = "01.00.01.50";
+      src = fetchFromGitHub {
+        owner = "bambulab";
+        repo = "BambuStudio";
+        rev = "v01.00.01.50";
+        hash = "sha256-7mkrPl2CQSfc1lRjl1ilwxdYcK5iRU//QGKmdCicK30=";
+      };
+    }))
+  ];
+}
--- modules/desktop/default.nix
+++ modules/desktop/default.nix
@@ -47,6 +47,13 @@ in
   ];
 
   services.flatpak.enable = true;
+  systemd.services.flatpak-repo = {
+    wantedBy = [ "multi-user.target" ];
+    path = [ pkgs.flatpak ];
+    script = ''
+      flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+    '';
+  };
 
   environment.systemPackages = stablePackages ++ unstablePackages;
 }
