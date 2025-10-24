1f9ded008e7856ddc28b0cd5fec1081f8f08fcac
Piloero <piloero@proton.me>
2025-10-24 19:06:30 +0200

    wip: shell with starship

--- systems/saturn/saturn.nix
+++ systems/saturn/saturn.nix
@@ -103,6 +103,8 @@
     useDefaultShell = true;
   };
 
+  environment.systemPackages = with pkgs; [ networkmanager-openvpn ];
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
