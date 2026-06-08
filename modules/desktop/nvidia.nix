87b46bcae3bb6d358a4a7fbeb90fb33034f3e2bf
Piloero <piloero@proton.me>
2026-06-08 22:34:44 +0200

    feat: update to 26.05 -> fix nvidia driver issue

--- modules/desktop/nvidia.nix
+++ modules/desktop/nvidia.nix
@@ -46,7 +46,7 @@
     nvidiaSettings = true;
 
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.latest;
+    package = config.boot.kernelPackages.nvidiaPackages.legacy_580;
   };
 
   environment.systemPackages = [
